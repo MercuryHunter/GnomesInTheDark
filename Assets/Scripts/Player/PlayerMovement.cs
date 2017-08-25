299096c194b368988cb1e03e65ea0def7f1c41ef
MercuryHunter <ujonathanalp@gmail.com>
2017-08-25 09:34:03 +0200

    Simple player movement script, and changed the level layout a bit

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,14 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
-
-	// Use this for initialization
+	
+	public float walkspeed = 2.5f;
+	public float runspeed = 6.0f;
+	
+	Rigidbody playerRigidbody;
+	
 	void Start () {
-			
+		// TODO: Put cursor stuff in mouse controller script?
+		Cursor.lockState = CursorLockMode.Locked;
+		
+		playerRigidbody = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		float moveHorizontal = Input.GetAxisRaw ("Horizontal");
+		float moveVertical = Input.GetAxisRaw ("Vertical");
+
+		Move (moveHorizontal, moveVertical);
+		Rotate();
+
+		if (Input.GetKeyDown ("escape")) {
+			Cursor.lockState = CursorLockMode.None;
+		}
+	}
+
+	private void Move(float moveHorizontal, float moveVertical) {
+		bool running = Input.GetKey (KeyCode.LeftShift);
+		float currentSpeed = ((running) ? runspeed : walkspeed);
 		
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		movement = movement.normalized * currentSpeed * Time.deltaTime;
+		
+		playerRigidbody.MovePosition (transform.position + movement);
+	}
+
+	private void Rotate() {
+		// TODO: Rotate according to camera?
 	}
 }

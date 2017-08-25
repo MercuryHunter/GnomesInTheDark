d2ffcd6eaf8a6dae574ebb0ba7ca1f58fe6ba684
MercuryHunter <ujonathanalp@gmail.com>
2017-08-25 12:06:15 +0200

    Player movement fix

---
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour {
+
+	Vector2 mouseLook;
+
+	Vector2 smoothV;
+
+	public float Sensitivity = 5.0f;
+	public float smoothing = 2.0f;
+
+	public GameObject target;
+
+	void Update () {
+		var camTurn = new Vector2 (Input.GetAxisRaw ("Mouse X"), Input.GetAxisRaw ("Mouse Y"));
+
+		camTurn = Vector2.Scale (camTurn, new Vector2 (Sensitivity * smoothing, Sensitivity * smoothing));
+		smoothV.x = Mathf.Lerp (smoothV.x, camTurn.x, 1f/ smoothing);
+		smoothV.y = Mathf.Lerp (smoothV.y, camTurn.y, 1f/ smoothing);
+		mouseLook += smoothV;
+
+		mouseLook.y = Mathf.Clamp (mouseLook.y, -90f, 90f);
+
+		transform.localRotation = Quaternion.AngleAxis (-mouseLook.y, Vector3.right);
+		target.transform.rotation = Quaternion.AngleAxis (mouseLook.x, target.transform.up);
+	}
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -22,7 +22,7 @@ public class PlayerMovement : MonoBehaviour {
 		float moveVertical = Input.GetAxisRaw ("Vertical");
 
 		Move (moveHorizontal, moveVertical);
-		Rotate();
+		// Rotation is done by the camera
 
 		if (Input.GetKeyDown ("escape")) {
 			Cursor.lockState = CursorLockMode.None;
@@ -33,13 +33,11 @@ public class PlayerMovement : MonoBehaviour {
 		bool running = Input.GetKey (KeyCode.LeftShift);
 		float currentSpeed = ((running) ? runspeed : walkspeed);
 		
-		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		Vector3 movement = transform.right * moveHorizontal + transform.forward * moveVertical;
 		movement = movement.normalized * currentSpeed * Time.deltaTime;
 		
 		playerRigidbody.MovePosition (transform.position + movement);
+	
 	}
 
-	private void Rotate() {
-		// TODO: Rotate according to camera?
-	}
 }

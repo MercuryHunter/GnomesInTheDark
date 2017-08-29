96773593621cfaf5c96b02b24fa261bf511cf758
MercuryHunter <ujonathanalp@gmail.com>
2017-08-29 21:51:07 +0200

    Added in teleportation - press e in a zone to teleport to the linked zone, with nice orientation too.

---
+++ Assets/Scripts/Player/PlayerTeleportation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTeleportation : MonoBehaviour {
+	// Handles the players use of teleporters
+	float teleportContactTimeRequired = 0.5f;
+	float teleportContactTimeCurrent = 0;
+
+	void OnTriggerStay(Collider other) {
+		// TODO: UI Text for player about this
+		teleportContactTimeCurrent -= Time.deltaTime;
+		
+		if (Input.GetKeyDown(KeyCode.E) && teleportContactTimeCurrent <= 0 && other.tag.Equals("Teleporter")) {
+			teleportContactTimeCurrent = teleportContactTimeRequired;
+			
+			Transform newTransform =
+				other.gameObject
+					.GetComponentInParent<TeleporterManager>()
+					.getOtherTeleporter(other)
+					.transform;
+
+			this.transform.position = newTransform.position;
+			this.transform.rotation = newTransform.rotation;
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.tag.Equals("Teleporter")) {
+			// Left teleporter, so need to recontact for appropriate time now
+			// May have issues with close together teleporters/edges of them
+			teleportContactTimeCurrent = teleportContactTimeRequired;
+		}
+	}
+}
---
+++ Assets/Scripts/TeleporterManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleporterManager : MonoBehaviour {
+
+	public Collider teleporter1, teleporter2;
+
+	public GameObject getOtherTeleporter(Collider entered) {
+		return teleporter1.Equals(entered) ? teleporter2.gameObject : teleporter1.gameObject;
+	}
+}

4544e5f31998fd65a7748d458641898e0a1be8da
Dylan S <dylansims3757@hotmail.co.za>
2017-08-31 10:27:06 -0700

    Added code tto Item and inventory, allow user to pick up item and now equip it. Make corections to equiping

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -18,13 +18,21 @@ public class Inventory : MonoBehaviour {
     private int numButtons = 3;
     private bool[] buttonShowing = new bool[3];
     int buttonOn = 0;
+    private GameObject[] holdingItems = new GameObject[3];
+    public GameObject item;
+    private bool isFull = false;
+    private int itemsInBag = 0;
+    public GameObject player;
 
     public void Start()
     {
+        
         for (int i = 0; i < numButtons; i++)
         {
             buttonShowing[i] = false;
+            holdingItems[i] = null;
         }
+        //holdingItems[0] = item;
     }
 
     public void Update()
@@ -42,31 +50,101 @@ public class Inventory : MonoBehaviour {
             }
             
         }
+        if (Input.GetKeyDown("e"))
+        {
+            if (!isFull) 
+                pickUpItem(item);
+        }
 
        
     }
 
     public void onItemClick(Button button)
     {
-        button.image.sprite = cogTexture;
-        for (int i = 0; i < numButtons; i++)
+        if (itemsInBag != 0)
         {
-          //  buttonShowing[i] = false;
-            GameObject.Find("Drop" + Convert.ToString(i + 1)).GetComponent<Image>().enabled = false;
+            for (int i = 0; i < numButtons; i++)
+            {
+                  buttonShowing[i] = false;
+                  GameObject.Find("Drop" + Convert.ToString(buttonOn + 1)).GetComponent<Image>().enabled = false;
+                GameObject.Find("Equip" + Convert.ToString(buttonOn + 1)).GetComponent<Image>().enabled = false;
+
+            }
+            buttonOn = Convert.ToInt32(button.name.Substring(button.name.Length - 1, 1)) - 1;
+            buttonShowing[buttonOn] = true;
+            if (holdingItems[buttonOn].GetComponent<Item>().itemType == Item.ItemType.UTILITY)
+            {
+                GameObject.Find("Equip" + Convert.ToString(buttonOn + 1)).GetComponent<Image>().enabled = true;
+            }
+            GameObject.Find("Drop" + Convert.ToString(buttonOn + 1)).GetComponent<Image>().enabled = true;
         }
-        buttonOn = Convert.ToInt32(button.name.Substring(button.name.Length-1, 1 ))-1;
-        //buttonShowing[buttonOn] = true;
-        GameObject.Find("Drop" + Convert.ToString(buttonOn+1)).GetComponent<Image>().enabled = true;
     }
 
     public void onDropClick(Button button)
     {
-        button.image.sprite = EmptyTexture;
-        GameObject.Find("Drop" + Convert.ToString(buttonOn + 1)).GetComponent<Image>().enabled = false;
+        int clickedButton = Convert.ToInt32(button.name.Substring(button.name.Length - 1, 1)) - 1;
+        if (buttonShowing[clickedButton])
+        {
+            print(buttonShowing[buttonOn]);
+            itemsInBag--;
+            button.image.sprite = EmptyTexture;
+            GameObject.Find("Drop" + Convert.ToString(buttonOn + 1)).GetComponent<Image>().enabled = false;
+            GameObject.Find("Equip" + Convert.ToString(buttonOn + 1)).GetComponent<Image>().enabled = false;
+            print(holdingItems[buttonOn]);
+            print(buttonOn);
+            dropItem(holdingItems[buttonOn].GetComponent<Item>());
+            holdingItems[buttonOn].transform.parent = null;
+            holdingItems[buttonOn] = null;
+            print(holdingItems[buttonOn]);
+            buttonShowing[clickedButton] = false;
+
+        }
     }
 
-    public void dropItem(GameObject item)
+    public void dropItem(Item item)
     {
+        item.setTransform(player.transform);
+    }
 
+    public void pickUpItem(GameObject gItem)
+    {
+        itemsInBag++;
+        gItem.GetComponent<Item>().pickUp();
+        for (int i = 0; i < numButtons; i++)
+        {
+            print("went throught" + i);
+            if (holdingItems[i] == null)
+            {
+                
+                holdingItems[i] = gItem;
+                switch (i){
+                    case 0:
+                        cog1.image.sprite = cogTexture;
+                        break;
+                    case 1:
+                        cog2.image.sprite = cogTexture;
+                        break;
+                    case 2:
+                        pick1.image.sprite = cogTexture;
+                        break;
+                }
+                //GameObject.Find("Slot" + Convert.ToString(i + 1));
+                //button.image.sprite = cogTexture;
+                break;
+            }
+            
+        }
+    }
+
+    public void onEquipClick(Button button)
+    {
+        int clickedButton = Convert.ToInt32(button.name.Substring(button.name.Length - 1, 1)) - 1;
+        if (buttonShowing[clickedButton])
+        {
+            Item item = holdingItems[buttonOn].GetComponent<Item>();
+            item.setTransform(player.transform);
+            item.getItem().transform.parent = player.transform;
+
+        }
     }
 }
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+
+    private Transform originalPosition;
+    private Transform currentLocation;
+    public bool inInventory;
+    public enum ItemType { COG, UTILITY};
+    public ItemType itemType;
+
+    private void Start()
+    {
+        originalPosition = transform;
+        currentLocation = transform;
+       // print(originalPosition);
+        inInventory = false;
+        itemType = ItemType.UTILITY;
+    }
+
+    public GameObject getItem()
+    {
+        return this.gameObject;
+    }
+
+    public void setTransform(Transform newLocation)
+    {
+        if (itemType == ItemType.UTILITY)
+        {
+
+        }
+        else
+        {
+            currentLocation = newLocation;
+            currentLocation.position = newLocation.position;
+        }
+        
+        //gameObject.GetComponent<Renderer>().enabled = false;
+        gameObject.SetActive(true);
+        inInventory = false;
+
+    }
+
+    public void pickUp()
+    {
+        gameObject.SetActive(false);
+        inInventory = true;
+    }
+}

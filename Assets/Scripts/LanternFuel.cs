4957a1436cff2f4eb1af0ccb0238255f544e9eb1
MercuryHunter <ujonathanalp@gmail.com>
2017-09-10 18:34:23 +0200

    Added in turning on and off light with key-press

--- Assets/Scripts/LanternFuel.cs
+++ Assets/Scripts/LanternFuel.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro.Examples;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class LanternFuel : MonoBehaviour {
 	private Light light;
@@ -12,34 +14,62 @@ public class LanternFuel : MonoBehaviour {
 	public float minRange = 8;
 	public float maxRange = 12;
 	public float maxFlickerDifference = 0.1f;
+
+	private bool on;
 	
 	void Start () {
 		light = GetComponent<Light>();
+		on = true;
 	}
 	
 	void Update () {
 		// TODO: Super flare
 		// TODO: Decrease in area last few seconds, extra flicker
-		fuel -= Time.deltaTime;
-
-		if (fuel <= 0) {
-			// Disable if out of fuel
-			fuel = 0;
-			light.enabled = false;
-			attachedParticleEmitter.SetActive(false);
-		}
-		else {
+		if (on) {
+			fuel -= Time.deltaTime;
+			
 			// Some flickering
 			// Tinker with variables at top to affect this.
 			float newRange = Random.Range(light.range - maxFlickerDifference, light.range + maxFlickerDifference);
 			light.range = Mathf.Clamp(newRange, minRange, maxRange);
+			
+			if (fuel <= 0) {
+				// Disable if out of fuel
+				fuel = 0;
+				turnOff();
+			}
+
+			if (Input.GetKeyDown(KeyCode.F)) turnOff();
+		}
+		else {
+			if (Input.GetKeyDown(KeyCode.F)) turnOn();
+		}
+	}
+
+	private void turnOff() {
+		on = false;
+		light.enabled = false;
+		attachedParticleEmitter.SetActive(false);
+	}
+
+	private void turnOn() {
+		if (fuel > 0) {
+			on = true;
+			light.enabled = true;
+			attachedParticleEmitter.SetActive(true);
 		}
 	}
 
 	public void addFuel(float amount) {
-		// Enable since we have fuel
-		fuel += amount;
-		light.enabled = true;
-		attachedParticleEmitter.SetActive(true);
+		// Enable since we have fuel if we didn't before
+		// This is a design choice, players who had 0 fuel had their light on before,
+		// so it makes sense to re-enable after getting more fuel 
+		if (fuel <= 0) {
+			fuel += amount;
+			turnOn();
+		}
+		else {
+			fuel += amount;
+		}
 	}
 }

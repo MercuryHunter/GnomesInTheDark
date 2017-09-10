f0a79d211e38b2a48ed30cdeb0a5fe07722b61a2
MercuryHunter <ujonathanalp@gmail.com>
2017-09-10 13:31:39 +0200

    Added in fire particle system for lantern, and a duststorm prefab

--- Assets/Scripts/LanternFuel.cs
+++ Assets/Scripts/LanternFuel.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class LanternFuel : MonoBehaviour {
 	private Light light;
 
+	public GameObject attachedParticleEmitter;
 	public float fuel = 10; // seconds of fuel
 	
 	void Start () {
@@ -20,11 +21,13 @@ public class LanternFuel : MonoBehaviour {
 		if (fuel <= 0) {
 			fuel = 0;
 			light.enabled = false;
+			attachedParticleEmitter.SetActive(false);
 		}
 	}
 
 	public void addFuel(float amount) {
 		fuel += amount;
-		light.enabled = true; 
+		light.enabled = true;
+		attachedParticleEmitter.SetActive(true);
 	}
 }

847562e3a7468f355710646fcdb75a21fbbd3fec
MercuryHunter <ujonathanalp@gmail.com>
2017-09-10 13:16:51 +0200

    Added in some lighting, and a lantern that has a rudimentary fuel system

---
+++ Assets/Scripts/LanternFuel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanternFuel : MonoBehaviour {
+	private Light light;
+
+	public float fuel = 10; // seconds of fuel
+	
+	void Start () {
+		light = GetComponent<Light>();
+	}
+	
+	void Update () {
+		// TODO: Flicker
+		// TODO: Super flare
+		// TODO: Decrease in area last few seconds, extra flicker
+		fuel -= Time.deltaTime;
+		
+		if (fuel <= 0) {
+			fuel = 0;
+			light.enabled = false;
+		}
+	}
+
+	public void addFuel(float amount) {
+		fuel += amount;
+		light.enabled = true; 
+	}
+}

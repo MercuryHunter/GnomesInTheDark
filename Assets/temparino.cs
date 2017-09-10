40dd4f7ad382707b85bab4c8bec6c90d2f4e2be3
MercuryHunter <ujonathanalp@gmail.com>
2017-09-10 23:20:28 +0200

    Tried to get wall texturing working... some walls still don't (dx = 0, dz != 0), something to do with uv (texture) coordinates of the mesh

---
+++ Assets/temparino.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class temparino : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+		Vector3[] vertices = mesh.vertices;
+		Vector2[] uvs = new Vector2[vertices.Length];
+
+		for (int i = 0; i < vertices.Length; i++)
+		{
+			Debug.Log("Vertices[" + i + "]: " + vertices[i]);
+
+			if (i > 0 && vertices[i].z - vertices[i - 1].z != 0 && vertices[i].x - vertices[i - 1].x == 0) {
+				if(vertices[i].y - vertices[i - 1].y == 0) 
+					uvs[i] = new Vector2(vertices[i].z, vertices[i].y * 10);
+				else
+					uvs[i] = new Vector2(vertices[i].y * 10, vertices[i].z);
+			}
+			else
+				uvs[i] = new Vector2(vertices[i].x, vertices[i].y);
+		}
+		
+		mesh.uv = uvs;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

8adacdbed189b6c46f55c59301e3479d022a6bcc
Dylan S <dylansims3757@hotmail.co.za>
2017-10-30 11:05:51 -0700

    Added extra sounds and a sounds controller that updates the sounds that play between 60 and 180 seconds. New sounds will be easy to add onces they are found

---
+++ Assets/Scripts/Objects/SoundsController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundsController : MonoBehaviour {
+
+    private float soundTimer;
+    private int timeSwitch;
+    public AudioSource[] allSources;
+
+    private void Start()
+    {
+        soundTimer = 0;
+        timeSwitch = Random.Range(60, 180);
+    }
+
+    private void Update()
+    {
+        soundTimer += Time.deltaTime;
+        if (soundTimer > timeSwitch)
+        {
+            soundTimer = 0;
+            timeSwitch = Random.Range(60, 180);
+            int selectedSource = Random.Range(0, 3);
+            allSources[selectedSource].Play();
+        }
+    }
+}
